bf59f3c6113ad7ca11bdf18fde11984d594962a2
Kato-Neko <ng112403@gmail.com>
2025-03-28 04:07:35 +0800

    landing page design

--- frontend_web/src/components/SearchBox.jsx
+++ frontend_web/src/components/SearchBox.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import "../styles/SearchBox.css";
+import { FaSearch, FaMapMarkerAlt, FaExchangeAlt } from "react-icons/fa";
 
 const SearchBox = () => {
   return (
     <div className="search-box">
       <h2>Where would you like to go today?</h2>
-      <div className="input-group">
-        <input type="text" placeholder="Current location or choose..." />
-        <input type="text" placeholder="Destination" />
+
+      <div className="input-container">
+        <div className="inputs">
+          <div className="input-box">
+            <FaMapMarkerAlt className="icon" />
+            <input type="text" placeholder="Current location or choose..." />
+          </div>
+
+          <div className="input-box">
+            <FaMapMarkerAlt className="icon" />
+            <input type="text" placeholder="Destination" />
+          </div>
+        </div>
+
+        <button className="swap-btn">
+          <FaExchangeAlt />
+        </button>
       </div>
-      <button className="search-btn">🔍 SEARCH</button>
+
+      <button className="search-btn">
+        <FaSearch className="search-icon" /> SEARCH
+      </button>
     </div>
   );
 };
--- frontend_web/src/pages/Home.jsx
+++ frontend_web/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MapView from "../components/MapView";
 import SearchBox from "../components/SearchBox";
+import TopSearchBar from "../components/TopSearchBar";
 
 const Home = () => {
   return (
     <div>
+      <TopSearchBar />
       <MapView />
       <SearchBox />
     </div>
