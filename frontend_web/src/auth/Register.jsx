46f001ce909a4b8670bc2089e26e65338ec5dab0
kiyoder <yoshinori.kyonojr@cit.edu>
2025-03-31 15:23:26 +0800

    register fix syntax

--- frontend_web/src/auth/Register.jsx
+++ frontend_web/src/auth/Register.jsx
@@ -54,84 +54,89 @@ function Register() {
         if (passwordStrength !== 'Strong') {
             setError('Password must be strong');
             return;
-
-        try {
-            const response = await axios.post('http://localhost:8080/api/users/register', {
-                username,
-                email,
-                password
-            });
-            console.log('Registration successful:', response.data);
-            navigate('/login');
-        } catch (error) {
-            console.error('Registration failed:', error);
-            setError(error.response ? error.response.data : 'Registration failed');
         }
-    };
+            try {
+                const response = await axios.post('http://localhost:8080/api/users/register', {
+                    username,
+                    email,
+                    password
+                });
+                console.log('Registration successful:', response.data);
+                navigate('/login');
+            } catch (error) {
+                console.error('Registration failed:', error);
+                setError(error.response ? error.response.data : 'Registration failed');
+            }
+        }
+        ;
 
-    return (
-        <div>
-            <h2>Register</h2>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input
-                        type={showPassword ? "text" : "password"} // Toggle input type
-                        value={password}
-                        onChange={handlePasswordChange}
-                        required
-                    />
+        return (
+            <div>
+                <h2>Register</h2>
+                {error && <p style={{color: 'red'}}>{error}</p>}
+                <form onSubmit={handleSubmit}>
                     <div>
+                        <label>Username</label>
                         <input
-                            type="checkbox"
-                            checked={showPassword}
-                            onChange={() => setShowPassword(!showPassword)}
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
                         />
-                        <label>Show Password</label>
                     </div>
-                    <div className="progress-bar">
-                        <div className="progress-segment" style={{ backgroundColor: getBarColor(passwordStrength, 0) }}></div>
-                        <div className="progress-segment" style={{ backgroundColor: getBarColor(passwordStrength, 1) }}></div>
-                        <div className="progress-segment" style={{ backgroundColor: getBarColor(passwordStrength, 2) }}></div>
+                    <div>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
                     </div>
-                    <div className="password-strength-text">
-                        <span style={{ color: passwordStrength === 'Weak' ? 'red' : passwordStrength === 'Medium' ? 'orange' : 'green' }}>
+                    <div>
+                        <label>Password</label>
+                        <input
+                            type={showPassword ? "text" : "password"} // Toggle input type
+                            value={password}
+                            onChange={handlePasswordChange}
+                            required
+                        />
+                        <div>
+                            <input
+                                type="checkbox"
+                                checked={showPassword}
+                                onChange={() => setShowPassword(!showPassword)}
+                            />
+                            <label>Show Password</label>
+                        </div>
+                        <div className="progress-bar">
+                            <div className="progress-segment"
+                                 style={{backgroundColor: getBarColor(passwordStrength, 0)}}></div>
+                            <div className="progress-segment"
+                                 style={{backgroundColor: getBarColor(passwordStrength, 1)}}></div>
+                            <div className="progress-segment"
+                                 style={{backgroundColor: getBarColor(passwordStrength, 2)}}></div>
+                        </div>
+                        <div className="password-strength-text">
+                        <span
+                            style={{color: passwordStrength === 'Weak' ? 'red' : passwordStrength === 'Medium' ? 'orange' : 'green'}}>
                             {passwordStrength}
                         </span>
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <label>Confirm Password</label>
-                    <input
-                        type={showPassword ? "text" : "password"} // Toggle input type
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Register</button>
-            </form>
-        </div>
-    );
+                    <div>
+                        <label>Confirm Password</label>
+                        <input
+                            type={showPassword ? "text" : "password"} // Toggle input type
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        );
 }
 
 export default Register;
