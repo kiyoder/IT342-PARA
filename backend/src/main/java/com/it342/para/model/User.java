d6f3210a00c12115b5a749aeac745d639f45a8e0
kiyoder <yoshinori.kyonojr@cit.edu>
2025-04-29 01:23:59 +0800

    comment user model should not be needed

--- backend/src/main/java/com/it342/para/model/User.java
+++ backend/src/main/java/com/it342/para/model/User.java
@@ -1,29 +1,29 @@
-package com.it342.para.model;
-
-
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-
-@Setter
-@Getter
-@Entity
-@Table(name = "users")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "supabase_uid", nullable = false, unique = true)
-    private String supabaseUid;
-
-    @Column(name = "username", nullable = false, unique = true)
-    private String username;
-
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
-
-    // Add created_at field if you want
-}
+//package com.it342.para.model;
+//
+//
+//import jakarta.persistence.*;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//
+//@Setter
+//@Getter
+//@Entity
+//@Table(name = "users")
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "supabase_uid", nullable = false, unique = true)
+//    private String supabaseUid;
+//
+//    @Column(name = "username", nullable = false, unique = true)
+//    private String username;
+//
+//    @Column(name = "email", nullable = false, unique = true)
+//    private String email;
+//
+//    // Add created_at field if you want
+//}
