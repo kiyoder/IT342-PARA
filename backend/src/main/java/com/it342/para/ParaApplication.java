4ef17546aa435522d975746dd5917c33bd3b612e
kiyoder <yoshinori.kyonojr@cit.edu>
2025-04-29 01:19:53 +0800

    remove springboot data jpa no longer needed conflict and disabled datasource configuration

--- backend/src/main/java/com/it342/para/ParaApplication.java
+++ backend/src/main/java/com/it342/para/ParaApplication.java
@@ -2,8 +2,15 @@ package com.it342.para;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class
+})
 public class ParaApplication {
 
 	public static void main(String[] args) {
