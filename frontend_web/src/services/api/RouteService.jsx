e050946129b1ca62333bffc18450066e6277b3e3
Kato-Neko <ng112403@gmail.com>
2025-04-29 23:48:56 +0800

    updated to use supabase api calls

--- frontend_web/src/services/api/RouteService.jsx
+++ frontend_web/src/services/api/RouteService.jsx
@@ -1,7 +1,7 @@
 // Service to handle route-related API calls and calculations
 
 // Base URL for API calls
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
 //token
 const authFetch = (url, opts = {}) => {
