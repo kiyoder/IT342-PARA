f52e48cb7b04a3b1748645a927f43315089b57fe
kiyoder <yoshinori.kyonojr@cit.edu>
2025-04-29 21:48:02 +0800

    url fix

--- frontend_web/src/services/AuthService.jsx
+++ frontend_web/src/services/AuthService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_BASE_API;
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Create axios instance with default config
 const api = axios.create({
