330c12b11b59eac19b6ea2da4e3f7be93c726867
kiyoder <yoshinori.kyonojr@cit.edu>
2025-05-01 21:55:03 +0800

    auth service ap fix

--- frontend_web/src/services/AuthService.jsx
+++ frontend_web/src/services/AuthService.jsx
@@ -2,18 +2,7 @@ import axios from "axios"
 
 const API_URL = import.meta.env.VITE_API_BASE_URL;
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token")
-        if (token) {
-            config.headers["Authorization"] = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
-    }
-)
+
 
 // Create axios instance with default config
 const api = axios.create({
@@ -34,7 +23,7 @@ api.interceptors.request.use(
     },
     (error) => {
         return Promise.reject(error)
-    },
+    }
 )
 
 // Auth service methods
