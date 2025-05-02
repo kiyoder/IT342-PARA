fafe877f822fe09918d7a8dbc63d31ad77be58c3
Kato-Neko <ng112403@gmail.com>
2025-05-02 15:24:22 +0800

    removing unwanted files

--- frontend_mobile/backend/src/main/java/com/it342/para/dto/GoogleLoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.it342.para.dto;
-
-import lombok.Data;
-
-@Data
-public class GoogleLoginRequest {
-    private String supabaseUid;
-}
--- frontend_mobile/backend/src/main/java/com/it342/para/dto/UserDTO.java
+++
@@ -1,7 +0,0 @@
-package com.it342.para.dto;
-
-public record UserDTO(
-        String supabaseUid,
-        String username,
-        String email
-) {}
