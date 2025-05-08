1fc10eba5e8402ce0101d9b6f82c0ac552c71a59
kiyoder <yoshinori.kyonojr@cit.edu>
2025-05-08 19:00:34 +0800

    google logo

--- frontend_web/src/components/auth/RegisterForm.jsx
+++ frontend_web/src/components/auth/RegisterForm.jsx
@@ -176,25 +176,22 @@ function RegisterForm({ onRegisterSuccess }) {
         </div>
 
         <button
-          type="button"
-          className="google-sign-in"
-          onClick={handleGoogleSignIn}
-          disabled={isLoading}
+            type="button"
+            className="google-sign-in"
+            onClick={handleGoogleSignIn}
+            disabled={isLoading}
         >
           {isLoading ? (
               "Redirecting..."
           ) : (
               <>
-                <svg viewBox="0 0 18 18" style={{ width: '24px', height: '24px' }}>
-                  <g>
-                    <path fill="#4285F4" d="M4.5 17.2h9c.4 0 .7-.3.7-.7V1.5c0-.4-.3-.7-.7-.7h-9c-.4 0-.7.3-.7.7v15c0 .4.3.7.7.7z"/>
-                    <path fill="#34A853" d="M17.3 9c0-.4-.3-.7-.7-.7H4.5c-.4 0-.7.3-.7.7v8.2c0 .4.3.7.7.7h12.1c.4 0 .7-.3.7-.7V9z"/>
-                    <path fill="#FBBC05" d="M4.5.8h9c.4 0 .7.3.7.7v8.2c0 .4-.3.7-.7.7H4.5c-.4 0-.7-.3-.7-.7V1.5c0-.4.3-.7.7-.7z"/>
-                    <path fill="#EA4335" d="M.7 9c0 .4.3.7.7.7h12.1c.4 0 .7-.3.7-.7V.8c0-.4-.3-.7-.7-.7H1.4c-.4 0-.7.3-.7.7V9z"/>
-                  </g>
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" style={{ marginRight: '8px' }}>
+                  <path d="M17.64 9.2045C17.64 8.5663 17.5827 7.9527 17.4764 7.3636H9V10.845H13.8436C13.635 11.97 13.0009 12.9231 12.0477 13.5613V15.8195H14.9564C16.6582 14.2527 17.64 11.9454 17.64 9.2045Z" fill="#4285F4"/>
+                  <path d="M9 18C11.43 18 13.4673 17.1941 14.9564 15.8195L12.0477 13.5613C11.2418 14.1013 10.2109 14.4204 9 14.4204C6.65591 14.4204 4.67182 12.8372 3.96409 10.71H0.957275V13.0418C2.43818 15.9831 5.48182 18 9 18Z" fill="#34A853"/>
+                  <path d="M3.96409 10.71C3.78409 10.17 3.68182 9.5931 3.68182 9C3.68182 8.4069 3.78409 7.83 3.96409 7.29V4.9582H0.957273C0.347727 6.1731 0 7.5477 0 9C0 10.4523 0.347727 11.8269 0.957273 13.0418L3.96409 10.71Z" fill="#FBBC05"/>
+                  <path d="M9 3.57955C10.3214 3.57955 11.5077 4.03364 12.4405 4.92545L15.0218 2.34409C13.4632 0.891818 11.4259 0 9 0C5.48182 0 2.43818 2.01682 0.957275 4.95818L3.96409 7.29C4.67182 5.16273 6.65591 3.57955 9 3.57955Z" fill="#EA4335"/>
                 </svg>
-                {/* You might still want to add text for accessibility or clarity */}
-                {/* <span>Sign in with Google</span> */}
+                Log in with Google
               </>
           )}
         </button>
